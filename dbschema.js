3f30d7f44722588c2987d702c82f459dc2a5f3cb
GreedoRen <greedo.it@gmail.com>
2019-04-26 18:41:03 +0300

    Add and get user profile details

--- dbschema.js
+++ dbschema.js
@@ -7,5 +7,41 @@ let db = {
 			likeCount: 5,
 			commentCount: 2
 		}
+	],
+
+	users: [
+		{
+			userID: 'fwjRjagTl4XsZIVW91SOLG7esrs2',
+			email: 'user@mail.com',
+			handle: 'user',
+			createAt: '2019-04-24T14:40:54.101Z',
+			imageUrl: '',
+			bio: 'Hello, my name is user',
+			website: 'https://user.com',
+			location: 'London, UK'
+		}
+	]
+};
+
+const userDetails = {
+	credentials: {
+		userId: 'fwjRjagTl4XsZIVW91SOLG7esrs2',
+		email: 'user@email.com',
+		handle: 'user',
+		createAt: '2019-04-24T14:40:54.101Z',
+		imageUrl: '',
+		bio: 'Hello, my name is user 2',
+		website: 'https://user2.com',
+		location: 'London2, UK'
+	},
+	likes: [
+		{
+			userHandle: 'user',
+			screamId: '6es6CGgsSbsEu53EO8BJ'
+		},
+		{
+			userHandle: 'user',
+			screamId: 'GIGMYbmjupcX0CboVrVd'
+		}
 	]
 };
